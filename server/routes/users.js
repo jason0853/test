dd6eca8b0a68e486d8fd5696a3714010bc858474
Jason <jason0853@gmail.com>
2017-04-04 16:50:24 +0900

    Adds client-side validation. Extract TextFieldGroup

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,51 +1,15 @@
 import express from 'express';
-import Validator from 'Validator';
-import isEmpty from 'lodash/isEmpty';
+import validateInput from  '../shared/validations/signup';
 
 let router = express.Router();
 
-function validateInput(data) {
-
-    let errors = {};
-
-    if (Validator.isNull(data.username)) {
-        errors.username = 'This field is required';
-    }
-
-    if (!Validator.isEmail(data.email)) {
-        errors.email = 'Email is invalid';
-    }
-
-    if (Validator.isNull(data.password)) {
-        errors.password = 'This field is required';
-    }
-
-    if (Validator.isNull(data.passwordConfirm)) {
-        errors.passwordConfirm = 'This field is required';
-    }
-
-    if (!Validator.equals(data.password, data.passwordConfirm)) {
-        errors.passwordConfirm = 'Passwords must match';
-    }
-
-    if (Validator.isNull(data.timezone)) {
-        errors.timezone = 'This field is required';
-    }
-
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
-
 router.post('/', (req, res) => {
-    setTimeout(() => {
-        const { errors, isValid } = validateInput(req.body);
+    const { errors, isValid } = validateInput(req.body);
 
-        if (!isValid) {
-            res.status(400).json(errors);
-        }
-    }, 5000);
+    if (!isValid) {
+        res.status(400).json(errors);
+    }
+
 
 });
 
---
+++ server/shared/validations/signup.js
@@ -0,0 +1,36 @@
+import Validator from 'Validator';
+import isEmpty from 'lodash/isEmpty';
+
+export default function validateInput(data) {
+
+    let errors = {};
+
+    if (Validator.isNull(data.username)) {
+        errors.username = 'This field is required';
+    }
+
+    if (!Validator.isEmail(data.email)) {
+        errors.email = 'Email is invalid';
+    }
+
+    if (Validator.isNull(data.password)) {
+        errors.password = 'This field is required';
+    }
+
+    if (Validator.isNull(data.passwordConfirm)) {
+        errors.passwordConfirm = 'This field is required';
+    }
+
+    if (!Validator.equals(data.password, data.passwordConfirm)) {
+        errors.passwordConfirm = 'Passwords must match';
+    }
+
+    if (Validator.isNull(data.timezone)) {
+        errors.timezone = 'This field is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
